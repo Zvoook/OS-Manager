0dffb512d337021682c3f205210c0002d7248fe7
SONJIWU28 <kipo30875@gmail.com>
2025-05-05 17:30:53 +0300

    Update

--- GameState.h
+++ GameState.h
@@ -1,15 +1,29 @@
 #pragma once
-#include "Resource.h"
 #include "Process.h"
+#include "Resource.h"
 class GameState {
 private:
-    vector<Resource> resources;
     vector<Process> processes;
+    vector<Resource> resources;
+    vector<vector<int>> table;
+    vector<int> available;
+    int level;
+    int wins;
+    int losses;
 public:
-    GameState() { initLevel1(); };
-    void initLevel1();
+    GameState() : level(1), wins(0), losses(0) { init_level(level); }
+    const vector<Process>& get_vec_pr() const { return processes; };
     const vector<Resource>& get_vec_res() const { return resources; };
-    const vector<Process>& get_vec_proc() const { return processes; };
-    void reset_res();
-    void reset_proc();
+    int get_lvl() const { return level; };
+    int get_wins() const { return wins; };
+    int get_losses() const { return losses; };
+    void init_level(int lvl);
+    //void design_level();
+    //bool make_request(int proc_id, int res_id, int k);
+    //bool deadlock_detect();
+    bool is_lvl_passed() const;
+    void next_lvl() {init_level(level + 1);};
+    void reset() { init_level(level); };
+    void add_wins() { wins++; };
+    void add_losses() { losses++; };
 };
