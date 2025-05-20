c67fd39e9a8f16be9d19b22d21c2c5a14ee98c37
Dmitry Danilov <rezviipersik@gmail.com>
2025-05-20 15:21:22 +0300

    UPD

--- Manager/Design.h
+++ Manager/Design.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "Constants.h"
-#include <string>
 #include <iostream>
+#include <string>
+#include "Constants.h"
+#include "GameState.h"
 using namespace std;
 using namespace sf;
 
 const Color white(255, 255, 255);
 const Color black(0, 0, 0);
-const Color blue(100, 200, 220);
-const Color white_red(10, 10, 10);
-const Color gray_blue(20, 40, 60);
-const Color dark_blue(10, 20, 40, 230);
-const Color beauty_red(227, 38, 54);
-const Color bordo(176, 0, 0);
+const Color blue(50, 150, 250);
+const Color red(227, 38, 54);
+const Color green(100, 200, 50);
+const Color yellow(255, 250, 205);
+const Color orange(255, 69, 0);
+
+const Color lightOrange(255, 140, 0);
+const Color lightRed(240, 128, 128);
+const Color lightBlue(131, 185, 229);
+const Color lightGreen(71, 167, 106);
+
+const Color darkGreen(0, 120, 0);
+const Color darkRed(120, 0, 0);
+
 
 class Frame:public Drawable {
 protected:
@@ -22,14 +31,12 @@ protected:
     Vector2f size, pos;
 public:
     Frame() = default;
-    Frame(const Font& font, const String& title, float s);
-    void setColor(sf::Color col) { back.setFillColor(col); }
+    Frame(const Font& font, const String& tite, float s);
+    void setColor(Color col, Color col2) { border.setFillColor(col); border.setOutlineColor(col2); }
     void draw(RenderTarget& target, RenderStates states) const override;
     void setSize(const Vector2f& size);
     void setPosition(const Vector2f& p);
-
-    float getBottom() const {return pos.y + size.y;}
-    float getRight() const {return pos.x + size.x;}
+    void setTextPos(float x, float y) { text.setPosition(x,y); }
 };
 
 class Button:public Drawable {
@@ -41,29 +48,62 @@ private:
 public:
     Button():isHovered(false), isActive(false) {};
     Button(const Font& font, const String& mes, float s);
-    void setColor(const sf::Color& col) { back.setFillColor(col); }
-    void hover(bool x);
+    void setColor(Color col, Color col2) { border.setFillColor(col); border.setOutlineColor(col2); }
+    void hover(bool x, Color self);
+    void hover(bool x, bool t);
     void activate(bool x);
     void draw(RenderTarget& target, RenderStates states) const override;
     bool contains(Vector2f point) const { return back.getGlobalBounds().contains(point); }
     void setSize(const Vector2f& size);
     void setPosition(const Vector2f& p);
 
-    float getBottom() const { return pos.y + size.y; }
-    float getRight() const { return pos.x + size.x; }
     bool get_hovered() const { return isHovered; }
+    bool get_active() const { return isActive; }
+    sf::Color getColor() const { return border.getFillColor(); }
+    void setTextPos(float x, float y) { text.setPosition(x, y); }
+};
+
+class Input {
+private:
+    wstring content;
+    Text text;
+    Button accept;
+    RectangleShape texture;
+    bool active;
+public:
+    Input() = default;
+    Input(const Font& font, Vector2f position, Vector2f size);
+
+    void handleEvent(const Event& event);
+
+    void draw(RenderWindow& window) const;
+    wstring getText() const { return content; }
+    bool is_empty() const { return content == ""; }
+    void clear() { content.clear(); text.setString(""); }
 };
 
 class LevelDesign {
 private:
     Frame main, resources, request, processes;
-    Button allow, deny;
+    Button grant, deny;
     RectangleShape background;
 public:
     LevelDesign(const Font& font);
     void draw(RenderWindow& w);
-    void reconstruct(int level);
-    void setFrameColor(const string& name, const sf::Color& color);
-    void interactive(const Vector2f& mousePos);
-    RectangleShape& get_background() { return background; }
+    void reconstruct(int l=0);
+    void setElemColor(const string& name, const Color& Col1, const Color& Col2);
+    int interactive(const Vector2f& mousePos, bool cl);
+};
+
+class Menu {
+private:
+    Frame main, info;
+    Button stats, l1, l2, l3, l4;
+    Input input;
+    RectangleShape background;
+public:
+    Menu() = default;
+    Menu(const Font& font);
+    void draw(RenderWindow& w);
+    int interactive(const Vector2f& mousePos);
 };
--- Manager/main.cpp
+++ Manager/main.cpp
@@ -1,79 +1,186 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <ctime>
+#include "Constants.h"
 #include "GameState.h"
 #include "Design.h"
-#include <iostream>
 using namespace sf;
 
-int main() {
-    Font font;
-    if (!font.loadFromFile("arial.ttf")) return 1;
-    GameState game;
-    RenderWindow window(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Resource Manager", Style::Close);
-    LevelDesign ui(font);
 
-    while (window.isOpen()) {
+string fix_str(const string& text, int width)
+{
+    if (text.length() >= size_t(width))
+        return text.substr(0, width - 1) + "~";
+    return text + string(width - text.length(), ' ');
+}
+void updateStatistic(Text& t, const GameState& g)
+{
+    t.setString("Level: " + to_string(g.get_lvl()) + "   Wins: " + to_string(g.get_wins()) + "   Losses: " + to_string(g.get_losses()));
+}
+Text create_res_text(int row, const Resource& r, Font& f)
+{
+    Text t("", f, 24);
+    t.setFillColor(black);
+    string line = fix_str(r.get_name(), RES_NAME_WIDTH) + SEP +
+        fix_str(to_string(r.get_available()) + "/" +
+            to_string(r.get_total()), CELL_WIDTH);
+    t.setString(line);
+    t.setPosition(FRAME_X_RES + 20,
+        FRAME_Y_RES + RES_OFFSET + row * RES_STEP);
+    return t;
+}
+Text create_proc_text(int row, int resCnt, const Process& p, Font& f)
+{
+
+    Text t("", f, 24);
+    t.setFillColor(black);
+    string line = fix_str(p.get_name(), PROC_NAME_WIDTH);
+    const auto& alloc = p.get_alloc();
+    const auto& max = p.get_max_require();
+
+    for (int i = 0; i < resCnt; ++i) {
+        int need = max[i] - alloc[i];
+        if (!need) line += fix_str("-", CELL_WIDTH);
+        else line += fix_str(to_string(need), CELL_WIDTH);
+    }
+    t.setString(line);
+    t.setPosition(FRAME_X_PROC + 20,
+        FRAME_Y_PROC + PROC_OFFSET + row * PROC_STEP);
+    return t;
+}
+Text makeHeader(const vector<string>& names, Font& f, int c)
+{
+    Text h("", f, 22);
+    h.setFillColor(black);
+    string line = fix_str("", PROC_NAME_WIDTH);
+    int i = 0;
+    for (const auto& n : names) if (i < c) {
+        line += fix_str(n, CELL_WIDTH);
+        ++i;
+    }
+    h.setString(line);
+    h.setPosition(FRAME_X_PROC + 48.5, FRAME_Y_PROC + PROC_OFFSET - 25);
+    return h;
+}
+
+int main()
+{
+    srand(time(0));
+    Font font;
+    Image icon;
+    if (!font.loadFromFile("Font.ttf") || !icon.loadFromFile("man.jpg")) return 1;
+
+    RenderWindow window(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Resource Manager", Style::Close);
+    GameState game;
+    LevelDesign ui(font);
+    Menu menu(font);
+    Text stat("", font, 20);  stat.setFillColor(black); stat.setPosition(57, 70);
+    Text info("", font, 21);  info.setFillColor(black); info.setPosition(430, 160);
+    Text menu_info("It's the OS operator game. This game simulate actual OS working.\nYou should grant or deny requests from processes and avoid deadlock.", font, 19);
+    menu_info.setFillColor(black);
+    menu_info.setPosition(Vector2f(INFO_X+30, INFO_Y+70));
+    bool  in_menu = true;
+
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    ui.reconstruct(game.get_lvl());
+
+    vector<string> res_names = { "CPU", "RAM", "DISK", "NET", "GPU" };
+    vector<Resource> resources = {
+        {0, "CPU",  8},
+        {1, "RAM", 16},
+        {2, "DISK", 6},
+        {3, "NET",  4},
+        {4, "GPU",  8}
+    };
+    vector<string> proc_names = { "Notepad", "Explorer", "Compiler", "Discord", "Minecraft", "Chrome" };
+    vector<Process> processes = {
+        {0, proc_names[0], {0, 2, 1, 1, 0}},
+        {1, proc_names[1], {2, 3, 2, 0, 0}},
+        {2, proc_names[2], {2, 3, 5, 3, 0}},
+        {3, proc_names[3], {5, 3, 3, 2, 0}},
+        {4, proc_names[4], {4, 5, 4, 3, 5}},
+        {5, proc_names[5], {3, 5, 6, 4, 1}}
+    };
+
+    int proc_count = START_PROC_COUNT + game.get_lvl() - 1;
+    game.set_manual(resources, processes, proc_count);
+    
+    Text header = makeHeader(res_names, font, proc_count);
+
+    Vector2f mouse;
+    bool waiting = false;
+    tuple<int, int, int> req{ -1,-1,-1 };
+    Clock clock;
+    float  last = 0;  int interval = 5 + rand() % 5;
+
+    while (window.isOpen())
+    {
         Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event))
+        {
             if (event.type == Event::Closed ||
-                (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)) {
+                (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape))
                 window.close();
+
+            if (event.type == Event::MouseMoved)
+                mouse = { float(event.mouseMove.x), float(event.mouseMove.y) };
+
+            if (!in_menu) {
+                if (event.type == Event::MouseButtonPressed)
+                {
+                    int state = ui.interactive(mouse, true);
+                    if (state && waiting)
+                    {
+                        auto [pid, rid, amt] = req;
+                        info.setString(state == 1 ? game.action_result(pid, rid, amt) : "Request denied!");
+                        waiting = false;
+                    }
+                }
+                else ui.interactive(mouse, false);
+                if (clock.getElapsedTime().asSeconds() - last >= interval)
+                {
+                    last = clock.getElapsedTime().asSeconds();
+                    interval = 3 + rand() % 7;
+
+                    if (!waiting)
+                    {
+                        req = game.create_random_request();
+                        auto [pid, rid, amt] = req;
+                        if (pid >= 0)
+                        {
+                            waiting = true;
+                            info.setString(proc_names[pid] + " requests " +
+                                to_string(amt) + " of " + res_names[rid]);
+                        }
+                    }
+                }
+                window.clear(white);
+                menu.draw(window);
+                updateStatistic(stat, game);
+                ui.draw(window);
+                window.draw(stat);
+                window.draw(info);
+                window.draw(header);
+                int row = 0;
+                for (const auto& r : game.get_vec_res())
+                    window.draw(create_res_text(row++, r, font));
+                row = 0;
+                for (const auto& p : game.get_vec_pr())
+                    window.draw(create_proc_text(row++, game.get_vec_res().size(), p, font));
+                window.display();
             }
-            else if (event.type == Event::MouseMoved) {
-                Vector2f mousePos(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y));
-                ui.interactive(mousePos);
+            else {
+                if (event.type == Event::MouseButtonPressed)
+                {
+                    int state = menu.interactive(mouse);
+                    if (state) game.reset();
+                }
+                window.clear(white);
+                menu.draw(window);
+                window.draw(menu_info);
+                window.display();
             }
         }
-
-        window.clear(Color::Blue);
-        ui.draw(window);
-        //window.draw(ui.get_background());
-        Text info;
-        info.setFont(font);
-        info.setString(L"Уровень: " + to_wstring(game.get_lvl()) +
-            L"   Успехов: " + to_wstring(game.get_wins()) +
-            L"   Провалов: " + to_wstring(game.get_losses()));
-        info.setCharacterSize(18);
-        info.setFillColor(Color(200, 200, 200));
-        info.setPosition(70, 70);
-        window.draw(info);
-
-        /*Text title("Resource Status", font, 24);
-        title.setPosition(20, 10);
-        title.setFillColor(Color::White);
-        window.draw(title);*/
-        /*int res_offset = 50;
-        int res_step = 30;
-        for (size_t i = 0; i < game.get_vec_res().size(); ++i) {
-            const auto& res = game.get_vec_res()[i];
-            Text text;
-            text.setFont(font);
-            text.setCharacterSize(20);
-            text.setFillColor(Color::Cyan);
-            text.setString(res.get_name() + ": " + to_string(res.get_available()) + "/" + to_string(res.get_total()));
-            text.setPosition(20, res_offset + i * res_step);
-            window.draw(text);
-        }*/
-        /*Text procTitle("Processes", font, 24);
-        procTitle.setPosition(20, 120);
-        procTitle.setFillColor(Color::White);
-        window.draw(procTitle);*/
-
-        /*int proc_offset = 160;
-        int proc_step = 25;
-        for (size_t i = 0; i < game.get_vec_pr().size(); ++i) {
-            const auto& proc = game.get_vec_pr()[i];
-            Text text;
-            text.setFont(font);
-            text.setCharacterSize(18);
-            text.setFillColor(Color::Green);
-            string line = proc.get_name() + "  ";
-            for (int require : proc.get_max_require()) line += "(" + to_string(require) + ") ";
-            text.setString(line);
-            text.setPosition(20, proc_offset + i * proc_step);
-            window.draw(text);
-        }*/
-        window.display();
     }
-
     return 0;
 }
