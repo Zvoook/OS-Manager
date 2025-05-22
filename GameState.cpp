d36e388d31237c33b6a1c04f33b4dfc9cc30d6ed
Dmitry Danilov <rezviipersik@gmail.com>
2025-05-22 14:15:57 +0300

    Update GameState.cpp

--- GameState.cpp
+++ GameState.cpp
@@ -1,431 +1,61 @@
-#include "GameState.h"
-#include <iostream>
+#pragma once
+#include "Process.h"
+#include "Resource.h"
+#include <vector>
+#include <string>
+#include <random>
+#include <map>
+#include <array>
 #include <fstream>
 #include <algorithm>
-#include <sstream>
-#include <tuple>
 
-void GameState::init_level(vector<Process>& pr, vector<string>& res_names, int pr_cnt[], int res_cnt[], vector<float>& coeff, int l) {
-    level = l;
-    processes.clear();
-    resources.clear();
-    allocation.clear();
-    available.clear();
-    int p_count = pr_cnt[level - 1];
-    int r_count = res_cnt[level - 1];
+class GameState {
+private:
+    std::string player_name;
+    std::map<std::string, std::array<int, 3>> rating;
 
-    int total_procs = pr.size();
-    int safe_count = min(p_count, total_procs);
-    processes.assign(pr.begin(), pr.begin() + safe_count);
-
-    vector<vector<int>> max_require;
-    for (int i = 0; i < safe_count; ++i) max_require.push_back(processes[i].get_max_require());
-
-    vector<int> min_need(r_count, 0);
-    for (int i = 0; i < r_count; ++i) {
-        for (int j = 0; j < p_count; ++j) {
-            if (j < safe_count && min_need[i] < max_require[j][i])
-                min_need[i] = max_require[j][i];
-        }
+    vector<Process> processes;
+    vector<Resource> resources;
+    vector<vector<int>> allocation;
+    vector<int> available;
+    int level, wins, losses;
+public:
+    GameState(vector<Process>& pr, vector<string>& res_names, int pr_cnt[], int res_cnt[],
+        vector<float>& coeff, int l = 1) : level(l), wins(0), losses(0)
+    {
+        init_level(pr, res_names, pr_cnt, res_cnt, coeff, level);
     }
 
-    vector<int> res_sums(r_count, 0);
-    for (const auto& m : max_require) {
-        for (int j = 0; j < r_count; ++j) res_sums[j] += m[j];
-    }
+    void init_level(vector<Process>& pr, vector<string>& res_names, int pr_cnt[], int res_cnt[], vector<float>& coeff, int l);
+    string action_result(int proc_id, int res_id, int k);
+    bool deadlock_detect();
+    bool is_lvl_passed() const;
+    tuple<int, int, int> create_random_request();
+    void set_level(int i) { level = i; }
+    void release_finished();
+    bool make_request(int proc_id, int res_id, int k);
 
-    for (int j = 0; j < r_count; ++j) {
-        bool used = false;
-        for (int i = 0; i < safe_count; ++i) {
-            if (processes[i].get_max_require()[j] > 0) {
-                used = true;
-                break;
-            }
-        }
-        if (used && res_sums[j] == 0) res_sums[j] = 1;
-    }
+    void add_wins() { wins++; }
+    void add_losses() { losses++; }
 
-    for (int i = 0; i < r_count; ++i) {
-        int base = static_cast<int>(res_sums[i] * coeff[level - 1]);
-        int variation = (rand() % 3) - 1;
-        int total = max(1, base + variation);
-        total = max(total, min_need[i]);
-        resources.emplace_back(i, res_names[i], total);
-        resources[i].set_available(total);
-        available.push_back(total);
-    }
-    allocation.assign(safe_count, vector<int>(r_count, 0));
-}
+    const vector<Process>& get_vec_pr() const { return processes; }
+    const vector<Resource>& get_vec_res() const { return resources; }
+    int get_lvl() const { return level; }
+    int get_wins() const { return wins; }
+    int get_losses() const { return losses; }
 
-bool GameState::is_lvl_passed() const {
-    for (int i = 0; i < processes.size(); ++i) {
-        if (!processes[i].is_done()) return false;
-    }
-    return true;
-}
+    void save_to_file(const string& filename) const;
+    bool load_from_file(const string& filename);
 
-string GameState::action_result(int proc_id, int res_id, int k) {
-    string mes;
-    Process& proc = processes[proc_id];
-    int need = proc.get_max_require()[res_id] - proc.get_alloc()[res_id];
+    void set_player(const string& name);
+    void save_rating(const string& path) const;
+    void load_rating(const string& path);
+    vector<tuple<string, int, int, int>> get_all_stats() const;
+    void add_win();
+    void add_loss();
+    void update_max_level(int lvl);
 
-    if (k > need || k > available[res_id]) return "";
-    available[res_id] -= k;
-    allocation[proc_id][res_id] += k;
-    proc.add_res(res_id, k);
-    resources[res_id].take(k);
-
-    if (deadlock_detect()) {
-        mes += "Resource successful granted";
-        if (proc.is_done()) {
-            for (int i = 0; i < resources.size(); ++i) {
-                int allocated = allocation[proc_id][i];
-                available[i] += allocated;
-                resources[i].back(allocated);
-                allocation[proc_id][i] = 0;
-            }
-            mes += "\n\nPROCESS COMPLETED";
-        }
-    }
-    else {
-        available[res_id] += k;
-        allocation[proc_id][res_id] -= k;
-        proc.add_res(res_id, -k);
-        resources[res_id].back(k);
-        mes += "You face to deadlock\n\nGAME OVER";
-    }
-    return mes;
-}
-
-bool GameState::deadlock_detect() {
-    int num_processes = processes.size();
-    int num_resources = resources.size();
-
-
-    vector<int> work = available;
-    vector<bool> Fillnish(num_processes, false);
-    vector<vector<int>> need(num_processes, vector<int>(num_resources));
-    for (int i = 0; i < num_processes; ++i) {
-        for (int j = 0; j < num_resources; ++j) {
-            need[i][j] = processes[i].get_max_require()[j] - allocation[i][j];
-        }
-    }
-    bool progress = true;
-    while (progress) {
-        progress = false;
-        for (int i = 0; i < num_processes; ++i) {
-            if (!Fillnish[i]) {
-                bool can_Fillnish = true;
-                for (int j = 0; j < num_resources; ++j) {
-                    if (need[i][j] > work[j]) {
-                        can_Fillnish = false;
-                        break;
-                    }
-                }
-                if (can_Fillnish) {
-                    for (int j = 0; j < num_resources; ++j) {
-                        work[j] += allocation[i][j];
-                    }
-                    Fillnish[i] = true;
-                    progress = true;
-                }
-            }
-        }
-    }
-
-    for (bool f : Fillnish) {
-        if (!f) {
-            return false;
-        }
-    }
-    return true;
-}
-
-tuple<int, int, int> GameState::create_random_request() {
-    int p_size = processes.size();
-    int tries = 20;
-
-    while (tries--) {
-        int pid = rand() % p_size;
-        if (processes[pid].is_done()) continue;
-
-        const auto& rest = processes[pid].get_rest();
-        for (int j = 0; j < rest.size(); ++j) {
-            if (rest[j] > 0 && available[j] > 0) {
-                int max_req = min(rest[j], available[j]);
-                int amount = 1 + rand() % max_req;
-                return { pid, j, amount };
-            }
-        }
-    }
-
-    return { -1, -1, -1 };
-}
-
-bool GameState::make_request(int proc_id, int res_id, int k) {
-    Process& proc = processes[proc_id];
-    int need = proc.get_max_require()[res_id] - proc.get_alloc()[res_id];
-
-    if (k > need || k > available[res_id])
-        return false;
-
-    available[res_id] -= k;
-    allocation[proc_id][res_id] += k;
-    proc.add_res(res_id, k);
-    resources[res_id].take(k);
-
-    if (deadlock_detect()) {
-        if (proc.is_done()) {
-            for (int i = 0; i < resources.size(); ++i) {
-                int alloc = allocation[proc_id][i];
-                available[i] += alloc;
-                resources[i].back(alloc);
-                allocation[proc_id][i] = 0;
-            }
-        }
-        return true;
-    }
-    else {
-        available[res_id] += k;
-        allocation[proc_id][res_id] -= k;
-        proc.add_res(res_id, -k);
-        resources[res_id].back(k);
-        return false;
-    }
-}
-
-void GameState::release_finished() {
-    for (int pid = 0; pid < processes.size(); ++pid) {
-        if (processes[pid].is_done()) {
-            for (int i = 0; i < resources.size(); ++i) {
-                int alloc = allocation[pid][i];
-                available[i] += alloc;
-                resources[i].back(alloc);
-                allocation[pid][i] = 0;
-            }
-        }
-    }
-}
-
-// Сохранение состояния игры в текстовый файл
-void GameState::save_to_file(const string& filename) const {
-    ofstream file(filename);
-    if (!file) return;
-
-    // Сохраняем базовую информацию
-    file << level << " " << wins << " " << losses << endl;
-
-    // Сохраняем доступные ресурсы
-    file << available.size() << endl;
-    for (int avail : available) {
-        file << avail << " ";
-    }
-    file << endl;
-
-    // Сохраняем матрицу allocation
-    file << processes.size() << " " << resources.size() << endl;
-    for (const auto& row : allocation) {
-        for (int val : row) {
-            file << val << " ";
-        }
-        file << endl;
-    }
-
-    // Сохраняем ресурсы
-    file << resources.size() << endl;
-    for (const auto& res : resources) {
-        file << res.get_id() << " ";
-        file << res.get_name() << " ";
-        file << res.get_total() << " ";
-        file << res.get_available() << endl;
-    }
-
-
-    // Сохраняем процессы
-    file << processes.size() << endl;
-    for (const auto& proc : processes) {
-        file << proc.get_id() << " ";
-        file << proc.get_name() << " ";
-
-        // Сохраняем max_require
-        const auto& max_req = proc.get_max_require();
-        file << max_req.size() << " ";
-        for (int req : max_req) {
-            file << req << " ";
-        }
-
-        // Сохраняем alloc
-        const auto& alloc = proc.get_alloc();
-        file << alloc.size() << " ";
-        for (int a : alloc) {
-            file << a << " ";
-        }
-
-        // Сохраняем статус завершения
-        file << proc.is_done() << endl;
-    }
-}
-
-// Загрузка состояния игры из текстового файла
-bool GameState::load_from_file(const string& filename) {
-    ifstream file(filename);
-    if (!file) return false;
-
-    // Загружаем базовую информацию
-    file >> level >> wins >> losses;
-
-    // Загружаем доступные ресурсы
-    int avail_size;
-    file >> avail_size;
-    available.resize(avail_size);
-    for (int i = 0; i < avail_size; ++i) {
-        file >> available[i];
-    }
-
-    // Загружаем матрицу allocation
-    int proc_count, res_count;
-    file >> proc_count >> res_count;
-    allocation.resize(proc_count, vector<int>(res_count));
-    for (int i = 0; i < proc_count; ++i) {
-        for (int j = 0; j < res_count; ++j) {
-            file >> allocation[i][j];
-        }
-    }
-
-    // Загружаем ресурсы
-    int res_size;
-    file >> res_size;
-    resources.clear();
-    for (int i = 0; i < res_size; ++i) {
-        int id, total, avail;
-        string name;
-        file >> id >> name >> total >> avail;
-        Resource res(id, name, total);
-        res.set_available(avail);
-        resources.push_back(res);
-    }
-
-    // Загружаем процессы
-    int proc_size;
-    file >> proc_size;
-    processes.clear();
-    for (int i = 0; i < proc_size; ++i) {
-        int id, max_size, alloc_size;
-        string name;
-        bool done;
-        file >> id >> name;
-        // Загружаем max_require
-        file >> max_size;
-        vector<int> max_req(max_size);
-        for (int j = 0; j < max_size; ++j) {
-            file >> max_req[j];
-        }
-        // Загружаем alloc
-        file >> alloc_size;
-        vector<int> alloc(alloc_size);
-        for (int j = 0; j < alloc_size; ++j) {
-            file >> alloc[j];
-        }
-        // Загружаем статус завершения
-        file >> done;
-        Process proc(id, name, max_req);
-        proc.set_alloc(alloc);
-        proc.set_done(done);
-        processes.push_back(proc);
-    }
-
-    return true;
-}
-
-void GameState::set_player(const string& name) {
-    player_name = name;
-    // Если игрок новый, добавим в map
-    if (rating.find(name) == rating.end())
-        rating[name] = { 0, 0, 1 }; // победы, поражения, макс. уровень (начинаем с 1)
-}
-
-void GameState::save_rating(const string& path) const {
-    // Читаем текущие данные из файла
-    map<string, array<int, 3>> existing;
-    ifstream infile(path);
-    if (infile.is_open()) {
-        string name;
-        int w, l, m;
-        while (infile >> name >> w >> l >> m) {
-            existing[name] = { w, l, m };
-        }
-        infile.close();
-    }
-
-    // Обновляем или добавляем текущего игрока
-    if (!player_name.empty()) {
-        existing[player_name] = rating.at(player_name);
-    }
-
-    // Перезаписываем весь файл
-    ofstream out(path, ios::trunc);
-    for (const auto& pair : existing) {
-        const string& name = pair.first;
-        const array<int, 3>& stats = pair.second;
-        out << name << " " << stats[0] << " " << stats[1] << " " << stats[2] << endl;
-    }
-}
-
-void GameState::load_rating(const string& path) {
-    ifstream file(path);
-    if (!file.is_open()) return;
-    rating.clear();
-    string name;
-    int wins, losses, max_lvl;
-    while (file >> name >> wins >> losses >> max_lvl) {
-        rating[name] = { wins, losses, max_lvl };
-    }
-}
-
-vector<tuple<string, int, int, int>> GameState::get_all_stats() const {
-    vector<tuple<string, int, int, int>> stats;
-    for (const auto& pair : rating) {
-        const string& name = pair.first;
-        const array<int, 3>& data = pair.second;
-        stats.emplace_back(name, data[0], data[1], data[2]); // имя, победы, поражения, уровень
-    }
-    // Сортировка по уровню (по убыванию), затем по победам (по убыванию)
-    sort(stats.begin(), stats.end(),
-        [](const auto& a, const auto& b) {
-            int level_a = get<3>(a), level_b = get<3>(b);
-            int wins_a = get<1>(a), wins_b = get<1>(b);
-            if (level_a != level_b) return level_a > level_b;
-            return wins_a > wins_b;
-        });
-    return stats;
-}
-
-void GameState::add_win() {
-    if (!player_name.empty()) {
-        rating[player_name][0]++;
-        wins++;
-    }
-}
-
-void GameState::add_loss() {
-    if (!player_name.empty()) {
-        rating[player_name][1]++;
-        losses++;
-    }
-}
-
-void GameState::update_max_level(int lvl) {
-    if (!player_name.empty()) {
-        rating[player_name][2] = max(rating[player_name][2], lvl + 1);
-    }
-}
-int GameState::get_max_available_level() const {
-    if (player_name.empty()) {
-        return 0;
-    }
-    auto it = rating.find(player_name);
-    if (it != rating.end()) {
-        return min(4, it->second[2]);
-    }
-    return 1;
-}
+    int get_max_available_level() const;
+    bool has_player() const { return !player_name.empty(); }
+    string get_player_name() const { return player_name; }
+};
